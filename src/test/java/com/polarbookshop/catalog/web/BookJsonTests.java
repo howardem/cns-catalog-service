511a9d2cfeb567a215a914e004327c98dd819015
Howard Egana <howard.cu@gmail.com>
2023-07-16 21:18:34 -0400

    ch6 - Package and Publish code for Github Actions

--- src/test/java/com/polarbookshop/catalog/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalog/web/BookJsonTests.java
@@ -38,7 +38,8 @@ class BookJsonTests {
 				"isbn": "1234567890",
 				"title": "Title",
 				"author": "Author",
-				"price": 9.90
+				"price": 9.90,
+				"publisher": "Polarsophia"  
 			}
 			""";
 
